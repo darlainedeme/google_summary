ee9e08f1d449840c270934d39ffbcc2ac23b6eb7
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-12-14 12:24:46 +0100

    Create web_app.py

---
+++ web_app.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import nltk
+import streamlit as st
+
+
+# Set the query that you want to search for
+sentence = st.sidebar.text_input('Write your research query:', value='government policies energy 2022') 
+
+# Use requests to search for the query on Google
+response = requests.get(f"https://www.google.com/search?q={sentence}")
+
+# Use BeautifulSoup to parse the HTML response
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find the relevant results on the page
+results = soup.find_all("h3")
+
+# Open each result in a new tab
+for result in results:
+    link = result.find("a")["href"]
+    requests.get(f"http://www.google.com{link}")
+
+# Use nltk to summarize the content of each page
+for result in results:
+    link = result.find("a")["href"]
+    response = requests.get(f"http://www.google.com{link}")
+    text = BeautifulSoup(response.text, "html.parser").get_text()
+    summary = nltk.summarize(text)
+    st.write(summary)
